5a5b64dff7a080643f0a6a81050698a60ac5d8c0
John Gaffney <john.gaffney@plastiq.com>
2019-06-14 10:18:44 -0400

    Initial mock websocket commit

--- web-clients/prototypes/leaderboard-v1/src/App.js
+++ web-clients/prototypes/leaderboard-v1/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ScorecardViewContainer } from './containers/ScorecardViewContainer';
+
+import { HOLE_6_PLAYER_1234_SCORE_UPDATE, HOLE_5_PLAYER_1234_SCORE_UPDATE } from './actions/ScorecardActions';
+
+import { Provider } from 'react-redux';
+
+import { scorecardStore } from './stores/ScorecardStore';
+
+scorecardStore.dispatch({ type: 'INIT' });
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={scorecardStore}>
+        <ScorecardViewContainer />
+      </Provider>
     </div>
   );
 }
+
+setTimeout(() => scorecardStore.dispatch(HOLE_6_PLAYER_1234_SCORE_UPDATE), 2000);
+
+//setTimeout(() => scorecardStore.dispatch(HOLE_5_PLAYER_1234_SCORE_UPDATE), 2000);
 
 export default App;
