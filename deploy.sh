c3ed2817bce3efb210e6085d91293523d904d027
John Gaffney <john.gaffney@plastiq.com>
2019-06-17 15:46:19 -0400

    Updating deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,15 @@
+ENVIRONMENT=$1
+
+echo 'DEV, QA, PROD' | grep -q ${ENVIRONMENT} 
+if [ $? -eq 0 ] ; then
+  echo ""
+else
+  echo "invalid environment parameter"
+  usage
+fi
+
+environment=`echo "${ENVIRONMENT}" | tr '[:upper:]' '[:lower:]'`
+
+echo "starting deployment to ${ENVIRONMENT} environment (${environment})"
+
 lerna run deploy --stream -- ${environment} || exit $?
--- services/leaderboard-service/api/deploy.sh
+++ services/leaderboard-service/api/deploy.sh
@@ -3,8 +3,6 @@
 # we do this because the prune has remove this deploy dependency.
 npm install serverless-offline --save-dev.
 
-environment=$1
+echo "deploying serivce to ${1} environment"
 
-echo "deploying serivce to ${environment} environment"
-
-serverless deploy --verbose --stage ${environment}
+serverless deploy --verbose --stage ${1}
