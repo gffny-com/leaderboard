97b00397416c212d62a7841c7e3cdfc42263a669
John Gaffney <john.gaffney@plastiq.com>
2019-10-19 10:32:57 -0400

    adding deploy script for function

---
+++ services/round-analysis-service/api/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# we do this because the prune has remove this deploy dependency.
+npm install serverless-offline --save-dev.
+
+echo "deploying serivce to ${1} environment"
+
+serverless deploy --force --verbose --stage ${1}
